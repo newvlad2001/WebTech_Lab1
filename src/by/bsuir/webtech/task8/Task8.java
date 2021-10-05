4c69a0d6890a6d2c2ed0ba4f5e5384016a2f3f53
newvlad2001 <newvlad2001@gmail.com>
2021-10-05 16:24:57 +0300

    Task 8 new impl.

--- src/by/bsuir/webtech/task8/Task8.java
+++ src/by/bsuir/webtech/task8/Task8.java
@@ -4,47 +4,32 @@ import by.bsuir.webtech.services.output.impl.ConsoleWriter;
 import by.bsuir.webtech.services.output.interfaces.Writer;
 
 import java.util.Arrays;
-import java.util.Stack;
 
 public class Task8 {
-    private static final double[] testSeq1 = {1, 2, 9, 12, 45};
+    private static final double[] testSeq1 = {-5, 3, 4, 12, 45, 56};
     private static final double[] testSeq2 = {3, 7, 10, 28, 46, 50};
 
     public static void main(String[] args) {
         Writer writer = new ConsoleWriter();
         int[] result = findPositions(testSeq1, testSeq2);
+
         writer.writeln(Arrays.toString(result));
     }
 
     public static int[] findPositions(double[] seq1, double[] seq2) {
-        var stack1 = initStack(seq1);
-        var stack2 = initStack(seq2);
-        var positions = new int[seq2.length];
-        int index = 0;
-        int currentPos = 0;
+        int[] positions = new int[seq2.length];
+        int seq2Index = 0;
+        int currPos = 0;
 
-        while (!stack1.empty() && !stack2.empty()) {
-            if (stack2.peek() < stack1.peek()) {
-                stack2.pop();
-                positions[index++] = currentPos;
-            } else {
-                stack1.pop();
+        for (int seq1Index = 0; seq1Index < seq1.length && seq2Index < seq2.length; seq1Index++, currPos++) {
+            if (seq2[seq2Index] < seq1[seq1Index]) {
+                positions[seq2Index++] = currPos;
+                seq1Index--;
             }
-            currentPos++;
         }
-        while (!stack2.empty()) {
-            stack2.pop();
-            positions[index++] = currentPos++;
+        while (seq2Index < seq2.length) {
+            positions[seq2Index++] = currPos++;
         }
         return positions;
-    }
-
-    private static Stack<Double> initStack(double[] seq) {
-        var stack = new Stack<Double>();
-
-        for (int i = seq.length - 1; i > -1; i--) {
-            stack.push(seq[i]);
-        }
-        return stack;
     }
 }
